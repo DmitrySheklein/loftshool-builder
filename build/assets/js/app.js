3e88123ea3c332396dead1d4e2e1bc58490c346a
DmitrySheklein <sheklein.dima@gmail.com>
2016-06-17 18:30:35 +0300

    Welcome page(add image,script)

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,8 +1,14 @@
-(function() {
-  'use strict';
+'use strict';
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oKSB7XHJcbiAgJ3VzZSBzdHJpY3QnO1xyXG5cclxuICBzZXRUaW1lb3V0KGZ1bmN0aW9uKCkge1xyXG4gICAgZG9jdW1lbnQucXVlcnlTZWxlY3RvcignLmdyZWF0aW5nX3BpY3R1cmUnKS5jbGFzc0xpc3QuYWRkKCdtLS1zaG93Jyk7XHJcbiAgfSwgMTAwMCk7XHJcbn0pKCk7Il0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
+$('.login').on("click", function(e){
+    event.preventDefault();
+    $('.block-info').css('display','none');
+    $(".user-authorize").css('display','block');
+});
+
+$('.home').on("click", function(e){
+    event.preventDefault();
+    $('.block-info').css('display','block');
+    $(".user-authorize").css('display','none');
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIndXNlIHN0cmljdCc7XHJcblxyXG4kKCcubG9naW4nKS5vbihcImNsaWNrXCIsIGZ1bmN0aW9uKGUpe1xyXG4gICAgZXZlbnQucHJldmVudERlZmF1bHQoKTtcclxuICAgICQoJy5ibG9jay1pbmZvJykuY3NzKCdkaXNwbGF5Jywnbm9uZScpO1xyXG4gICAgJChcIi51c2VyLWF1dGhvcml6ZVwiKS5jc3MoJ2Rpc3BsYXknLCdibG9jaycpO1xyXG59KTtcclxuXHJcbiQoJy5ob21lJykub24oXCJjbGlja1wiLCBmdW5jdGlvbihlKXtcclxuICAgIGV2ZW50LnByZXZlbnREZWZhdWx0KCk7XHJcbiAgICAkKCcuYmxvY2staW5mbycpLmNzcygnZGlzcGxheScsJ2Jsb2NrJyk7XHJcbiAgICAkKFwiLnVzZXItYXV0aG9yaXplXCIpLmNzcygnZGlzcGxheScsJ25vbmUnKTtcclxufSk7Il0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,13 @@
-(function() {
-  'use strict';
+'use strict';
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+$('.login').on("click", function(e){
+    event.preventDefault();
+    $('.block-info').css('display','none');
+    $(".user-authorize").css('display','block');
+});
+
+$('.home').on("click", function(e){
+    event.preventDefault();
+    $('.block-info').css('display','block');
+    $(".user-authorize").css('display','none');
+});
