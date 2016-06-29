f45bc9d91941cd52ba3b1a43c3b6f0ec06c1f57a
DmitrySheklein <sheklein.dima@gmail.com>
2016-06-29 23:40:11 +0300

    Adaptive edit

--- source/js/app.js
+++ source/js/app.js
@@ -3,7 +3,9 @@
 $('.login').on("click", function(e){
     event.preventDefault();
     $('.block-info').css('display','none');
+    $('.user-card').addClass('flipped');
     $(".user-authorize").css('display','block');
+    // $('.login').css('display','none');
 });
 
 $('.home').on("click", function(e){
@@ -19,6 +21,8 @@ $('.sandwich').on("click", function(e){
     event.preventDefault();
     $('.sandwich').css('display','none');
     $('#menu').css('display','block');
+    $('.menu').addClass('active');
+    $('.nav__list').addClass('nav__list_show');
 });
 
 $('#btn_close').on("click", function(e){
