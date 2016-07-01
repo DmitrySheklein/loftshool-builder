7284681e2f7f2d6609f5189649a8abdfec293c14
DmitrySheklein <sheklein.dima@gmail.com>
2016-07-01 14:13:20 +0300

    Welcome page edit. Flip card

--- source/js/app.js
+++ source/js/app.js
@@ -1,33 +1,45 @@
 'use strict';
 
-$('.login').on("click", function(e){
-    event.preventDefault();
-    $('.block-info').css('display','none');
-    $('.user-card').addClass('flipped');
-    $(".user-authorize").css('display','block');
-    // $('.login').css('display','none');
-});
+// FLIP CARD
 
-$('.home').on("click", function(e){
-    event.preventDefault();
-    $('.block-info').css('display','block');
-    $(".user-authorize").css('display','none');
-});
+(function() {
+    $('.login').on('click', function(event) {
+        event.preventDefault();
+        $('.block-info').addClass('flip-cart');
+        $('.user-authorize').addClass('flip-entry');
+        $('.login').css('display','none');
+    });
+
+    $('.home-link').on('click', function(event) {
+        event.preventDefault();
+        $('.block-info').toggleClass('flip-cart');
+        $('.user-authorize').toggleClass('flip-entry');
+        $('.login').css('display','block');
+    });
+
+    $(document).on('keyup', function(close) {
+        if (close.keyCode == 27) {
+            $('.block-info').removeClass('flip-cart');
+            $('.user-authorize').removeClass('flip-entry');
+            $('.login').css('display','block');
+        }
+    });
+})();
 
 
+// MENU
+(function () {
+    $('.sandwich').on("click", function(e){
+        event.preventDefault();
+        $('.sandwich').css('display','none');
+        $('#menu').css('display','block');
+        $('.menu').addClass('active');
+        $('.nav__list').addClass('nav__list_show');
+    });
 
-//Меню
-$('.sandwich').on("click", function(e){
-    event.preventDefault();
-    $('.sandwich').css('display','none');
-    $('#menu').css('display','block');
-    $('.menu').addClass('active');
-    $('.nav__list').addClass('nav__list_show');
-});
-
-$('#btn_close').on("click", function(e){
-    event.preventDefault();
-    $('#menu').css('display','none');
-    $('.sandwich').css('display','block');
-});
-
+    $('#btn_close').on("click", function(e){
+        event.preventDefault();
+        $('#menu').css('display','none');
+        $('.sandwich').css('display','block');
+    });
+})();
--- source/js/map.js
+++ source/js/map.js
@@ -9,16 +9,88 @@
         map = new google.maps.Map(document.getElementById('map'), {
             disableDefaultUI: true,
             scrollwheel: false,
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 12
+            styles: [
+                {
+                    "featureType": "administrative",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#444444"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "color": "#f2f2f2"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 45
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "color": "#1056d1"
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                }
+            ],
+            center: {lat: 56.295, lng: 44},
+            zoom: 13
         });
     }
-});
-
-// var map;
-// function initMap() {
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: {lat: -34.397, lng: 150.644},
-//         zoom: 8
-//     });
-// }
+})();
