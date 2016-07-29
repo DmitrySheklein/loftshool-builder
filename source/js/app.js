aac5a165062db5cb8228cfac1aa867099553dfac
DmitrySheklein <sheklein.dima@gmail.com>
2016-07-29 17:37:32 +0300

    Sidebar edit/ Preloader add

--- source/js/app.js
+++ source/js/app.js
@@ -29,7 +29,7 @@
 
 // // MENU
 //
-
+(function () {
 var bgMenu = $('.nav_menu'),
     content = $('.nav_content'),
     sandwich = $('.sandwich'),
@@ -54,9 +54,11 @@ function onClickClose(e){
     sandwich.show();
     close_bnt.hide();
 }
+})();
+
 // Blog-menu
 //====================
-
+(function () {
 (function () {
     if($('.static .blog__list').length){
         var menu = $('.static .blog__list'),
@@ -82,16 +84,18 @@ function onClickClose(e){
 
 })();
 
-// $(window).scroll(function () {
-//     // checkSection();
-//     var sticky = $('.blog__themes'),
-//         scroll = $(window).scrollTop();
-//     if($('.blog__themes').length > 0) {
-//         if (scroll > $('.blog__themes').offset().top) sticky.addClass('fixed');
-//         else sticky.removeClass('fixed');
-//     }
-// });
 
+var sidebar = $('.blog__themes_sidebar'),
+    sidebar_btn = $('.sidebar_btn');
+
+sidebar_btn.on('click',sidebar_active);
+
+function sidebar_active(e) {
+    e.preventDefault();
+    sidebar.toggleClass('sidebar_active');
+    sidebar_btn.toggleClass('sidebar_btn_active');
+}
+})();
 //===============
 // Paralax
 //===============
@@ -202,3 +206,79 @@ $(window).on('mousemove', function (e) {
         slideItems.eq(nextImgIndex).addClass('slider_item_active');
     }
 })();
+
+
+// PRELOADER
+//=======================================================
+$(document).ready(function() {
+    $('.preloader').removeClass('preloader-noJs');
+    $('body').css('overflow', 'hidden');
+    $('body').bind('touchmove', function(e){e.preventDefault()});
+
+    var imgs = [];
+
+    $.each($('*'), function() {
+        var $this = $(this),
+            background = $this.css('background-image'),
+            img = $this.is('img');
+
+        if(background != 'none') {
+            var path = background.replace('url("', '').replace('")', '');
+            imgs.push(path);
+        }
+        if(img) {
+            var path = $this.attr('src');
+            if (path) {
+                imgs.push(path);
+            }
+        }
+    });
+
+    var percents = 1,
+        lastRealPercent = 0;
+
+    for (var i = 0; i < imgs.length; i++) {
+        var image = $('<img>', {
+            attr: {
+                src: imgs[i]
+            }
+        });
+
+        image.load(function() {
+            setPercents(imgs.length, percents);
+            percents++;
+        });
+    }
+
+    function setPercents(total, current) {
+        var percent = Math.ceil(current / total * 100);
+
+        var iterator = setInterval(function() {
+            if(lastRealPercent >= percent) {
+                clearInterval(iterator);
+                iterator = null;
+                return;
+            }
+
+            var dash = Math.ceil(316 - lastRealPercent * 1.58);
+            $('.circle-bar').css({'stroke-dashoffset': dash + 'px'});
+            $('.circle-txt').text(lastRealPercent);
+
+            if (percent >= 100) {
+                $('.circle-small').css('animation-iteration-count', '1');
+            };
+
+            lastRealPercent++;
+        }, 5);
+    }
+});
+
+
+$(window).load(function() {
+    $('body').css('overflow', 'visible');
+    $('body').unbind('touchmove');
+
+    $('.preloader-inner').fadeOut(1000, function() {
+        $('.preloader').css('display', 'none');
+    });
+});
